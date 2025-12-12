ffa1bcb64a5cae803b2499698cc7b5660ce7b7dd
Noah Oblath <nsoblath@gmail.com>
2025-12-11 18:07:19 -0800

    Print on thread_pool construction & destruction

--- include/spdlog/details/thread_pool-inl.h
+++ include/spdlog/details/thread_pool-inl.h
@@ -10,6 +10,8 @@
 #include <cassert>
 #include <spdlog/common.h>
 
+#include <iostream>
+
 namespace spdlog {
 namespace details {
 
@@ -18,6 +20,7 @@ SPDLOG_INLINE thread_pool::thread_pool(size_t q_max_items,
                                        std::function<void()> on_thread_start,
                                        std::function<void()> on_thread_stop)
     : q_(q_max_items) {
+        std::cerr << "thread_pool constructor" << std::endl;
     if (threads_n == 0 || threads_n > 1000) {
         throw_spdlog_ex(
             "spdlog::thread_pool(): invalid threads_n param (valid "
@@ -42,6 +45,7 @@ SPDLOG_INLINE thread_pool::thread_pool(size_t q_max_items, size_t threads_n)
 
 // message all threads to terminate gracefully join them
 SPDLOG_INLINE thread_pool::~thread_pool() {
+    std::cerr << "thread_pool destructor" << std::endl;
     SPDLOG_TRY {
         for (size_t i = 0; i < threads_.size(); i++) {
             post_async_msg_(async_msg(async_msg_type::terminate), async_overflow_policy::block);
